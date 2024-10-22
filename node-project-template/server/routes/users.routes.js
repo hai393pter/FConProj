9e42228d47e153521c8878916af46464233d6c77
AnataAria <kakata28419@gmail.com>
2024-10-23 01:33:26 +0700

    fix router doc to user

--- node-project-template/server/routes/users.routes.js
+++ node-project-template/server/routes/users.routes.js
@@ -107,7 +107,7 @@ usersRouter.get('/me', checkAuth, userControllers.getMe);
  * /users/change-password:
  *   post:
  *     tags:
- *       - Authentication
+ *       - User
  *     summary: Change password for the logged-in user
  *     security:
  *       - bearerAuth: []
@@ -139,7 +139,7 @@ usersRouter.post('/change-password', checkAuth, userControllers.changePassword);
  *     security:
  *       - bearerAuth: []
  *     tags:
- *       - Authentication
+ *       - User
  *     requestBody:
  *       required: true
  *       content:
@@ -176,7 +176,7 @@ usersRouter.post('/forgot-password', userControllers.forgotPassword);
  *   post:
  *     summary: Đặt lại mật khẩu
  *     tags:
- *       - Authentication
+ *       - User
  *     requestBody:
  *       required: true
  *       content:
@@ -216,7 +216,7 @@ usersRouter.post('/reset-password', userControllers.resetPassword);
  *   put:
  *     summary: Cập nhật thông tin người dùng
  *     tags:
- *       - Authentication
+ *       - User
  *     requestBody:
  *       required: true
  *       content:
